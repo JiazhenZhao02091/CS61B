e20b515a6f736374dc154e5ba0f4733ad0be7eb6
JiazhenZhao02091 <873694994@qq.com>
2024-01-22 12:32:11 +0800

    change dir about submit test

---
+++ proj0/Planet.java
@@ -0,0 +1,157 @@
+
+/**
+ * @author JiazhenZhao
+ * 2023/12/15
+ * 类说明：
+ */
+public class Planet {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    /**
+     * 构造函数，如果参数选择和变量一样的名字，必须要使用 this keyword
+     * eg: this.mass = mass;
+     */
+    public Planet(double xP, double yP, double xV, double yV, double m, String img){
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass  = m;
+        imgFileName = img;
+    }
+
+    public Planet(Planet b){
+        xxPos = b.xxPos;
+        yyPos = b.yyPos;
+        xxVel = b.xxVel;
+        yyVel = b.yyVel;
+        mass = b.mass;
+        imgFileName = b.imgFileName;
+    }
+
+    public Planet() {
+    }
+
+    /**
+     * returns a double var that mean this planet and the 'p' planet distance.
+     * @param p the other planet.
+     * @return r mean the distance.
+     */
+    public double calcDistance(Planet p){
+        double r = (xxPos - p.xxPos) * (xxPos - p.xxPos) + (yyPos - p.yyPos) * (yyPos - p.yyPos);
+        r = Math.sqrt(r);
+        return r;
+    }
+
+    /**
+     * returns a double var, the force between This planet and the Planet p.
+     * @param p other planet p.
+     * @return force between the Planet ant the Planet p.
+     * calc formula: F = G * m1 * m2 / r^2;=6.67⋅10 -11
+     */
+    public double calcForceExertedBy(Planet p){
+        double F = mass * p.mass;
+        double r = calcDistance(p);
+        double G = 6.67e-11;
+        F = F * G / (r * r);
+        return F;
+    }
+
+    /**
+     * returns the force in x direction.
+     * @return
+     * F = f * dx / r
+     */
+    public double calcForceExertedByX(Planet p){
+        double r = calcDistance(p);
+        double F = calcForceExertedBy(p);
+        double dx = p.xxPos - xxPos;
+        double ForceByX = F * dx / r;
+        return ForceByX;
+    }
+
+    /**
+     * returns the force in y direction.
+     * @param p other planet
+     * @return
+     * F = f * dy / r
+     */
+    public double calcForceExertedByY(Planet p){
+        double r = calcDistance(p);
+        double F = calcForceExertedBy(p);
+        double dy = p.yyPos - yyPos;
+        double ForceByY = F * dy / r;
+        return ForceByY;
+    }
+
+    /**
+     * return the all net force in x for This planet.
+     * @param planets all planets.
+     * @return the force in x direction.
+     */
+    public double calcNetForceExertedByX(Planet[] planets){
+        double ForceX = 0;
+        for(int i = 0 ; i < planets.length ; i ++)
+        {
+            if(planets[i].equals(this))
+                continue;
+            ForceX += this.calcForceExertedByX(planets[i]);
+        }
+        return ForceX;
+    }
+
+    /**
+     * return the all net force in 'y' for This planet.
+     * @param planets all planets.
+     * @return the force in 'y' direction.
+     */
+    public double calcNetForceExertedByY(Planet[] planets){
+        double  ForceY = 0 ;
+        for(int i = 0 ; i < planets.length ; i ++)
+        {
+            if(planets[i].equals(this))
+                continue;
+            ForceY += this.calcForceExertedByY(planets[i]);
+        }
+        return ForceY;
+    }
+
+    /**
+     * update This planet bt the time and x of net force, y of net force.
+     * @param time consumer time.
+     * @param xNet x of net force.
+     * @param yNet y of net force.
+     */
+    public void update(double time, double xNet, double yNet){
+        double ax = xNet / mass;
+        double ay = yNet / mass;
+        xxVel = xxVel + ax * time;
+        yyVel = yyVel + ay * time;
+        xxPos = xxPos + xxVel * time;
+        yyPos = yyPos + yyVel * time;
+    }
+
+    /**
+     * return the radius of fileName.
+     * @param fileName file name.
+     * @return radius.
+     */
+    public double readRadius(String fileName){
+        In in = new  In(fileName);
+        int rank = in.readInt();
+        double radius = in.readDouble();
+        return radius;
+    }
+
+    /**
+     * draw a planet where it's position
+     */
+    public void draw(){
+        StdDraw.picture(xxPos,yyPos,"./images/" + imgFileName);
+    }
+}
