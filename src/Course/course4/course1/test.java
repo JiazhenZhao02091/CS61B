6063b91a33c873bd000d33fa87fb00cded7f4bac
JiazhenZhao02091 <873694994@qq.com>
2024-01-26 11:48:14 +0800

    done course 4.2

---
+++ src/Course/course4/course1/test.java
@@ -0,0 +1,39 @@
+package Course.course4.course1;
+
+/**
+ * @author JiazhenZhao
+ * 2024/1/26
+ * 类说明：
+ */
+public class test implements List61B{
+
+    @Override
+    public void addLast(Object x) {
+
+    }
+
+    @Override
+    public Object getLast() {
+        return null;
+    }
+
+    @Override
+    public Object get(int i) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Object removeLast() {
+        return null;
+    }
+
+    @Override
+    public void dy_resize() {
+
+    }
+}
---
+++ src/Course/course4/course2/main.java
@@ -0,0 +1,18 @@
+package Course.course4.course2;
+
+/**
+ * @author JiazhenZhao
+ * 2024/1/26
+ * 类说明：
+ */
+public class main {
+    public static void main(String[] args) {
+
+
+    }
+}
+/**
+ *  子类继承父类之后，再调用方法的过程中，编译器只允许根据静态类型做事
+ *  针对于重写方法（Override）实际调用的方法基于  **动态类型**
+ *      如果没重写就会出现Complie error ，即使子类中具有该方法
+ */
